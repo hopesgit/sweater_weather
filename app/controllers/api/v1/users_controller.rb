9a1c255197c0dadb7511b7346c36ed60d72f95ad
Hope Gochnour <hope.gochnour@gmail.com>
2021-01-17 18:09:19 -0700

    Add exception for password_confirmation missing and more edge testing

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.new(
-      email: user_params[:email],
-      password: user_params[:password],
-      password_confirmation: user_params[:password_confirmation],
-      api_key: ApiKeyCreator.generate_api_key
-    )
-
-    user.save
+    if user_params[:password_confirmation].nil?
+      raise StandardError.new "Password confirmation missing".to_json
+    else
+      User.create!(
+        email: user_params[:email],
+        password: user_params[:password],
+        password_confirmation: user_params[:password_confirmation],
+        api_key: ApiKeyCreator.generate_api_key
+      )
+    end
   end
 
   private
